e5a4656913f8e448572a77543745f09390062681
Prasanth Louis <prasanthlouis21@gmail.com>
2022-02-06 14:16:17 -0500

    Use ref() vanilla tilt

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -5,8 +5,12 @@ import * as React from 'react'
 
 function Board() {
   // 🐨 squares is the state for this component. Add useState for squares
-  const [squares, setSquares] = React.useState(Array(9).fill(null));
+  const [squares, setSquares] = React.useState(() => JSON.parse(window.localStorage.getItem('squares') || Array(9).fill(null)));
 
+
+  React.useEffect(() =>{
+    window.localStorage.setItem('squares', JSON.stringify(squares));
+  }, [squares])
   // 🐨 We'll need the following bits of derived state:
   const nextValue = calculateNextValue(squares);
   const winner = calculateWinner(squares);
